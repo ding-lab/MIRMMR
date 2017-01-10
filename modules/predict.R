37f776757201a53ccaba0703d609418dd81c7d91
envest <stevenmasonfoltz@gmail.com>
2017-01-10 15:43:11 -0600

    Deleted predict.R

--- modules/predict.R
+++
@@ -1,3 +0,0 @@
-#Module predicts MSI-H status given a model and new data frame
-Given model object (?) be able to predict MSI outcome on NEW data
-Need to wait to write other modules before knowing how to do this one
