2898fea4cf81208582dde9efc4a7dba894196dd2
envest <stevenmasonfoltz@gmail.com>
2017-01-10 12:37:19 -0600

    pseudocode written

---
+++ modules/stepwise.R
@@ -0,0 +1,12 @@
+#Module returns best model using stepwise regression
+msi_column
+first_parameter
+direction (default="both")
+require(MASS)
+if(msi_column is binary){
+  write model statement
+  evalute entire model
+  run stepAIC()
+  return best model and predicted values
+}
+
