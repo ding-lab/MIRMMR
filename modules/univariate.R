de3ab84ccfe1753eba153607351ae8d8bf876021
envest <stevenmasonfoltz@gmail.com>
2017-01-11 11:36:20 -0600

    Wrote real code functions

--- modules/univariate.R
+++ modules/univariate.R
@@ -1,20 +1,32 @@
 #Module returns univariate model p-values for each parameter in data frame
-msi_column
-first_parameter
+col <- opt$msi_score
+fdc <- opt$first_data_column
 
-#Pseudo code
-results_df <- EMPTY
-parameter_names <- df[,first_paramter:end]
-if( msi_column type is 0/1 or TRUE/FALSE or two factors ){
-  for(name in parameter_names){
-    check name is numeric
-    results_df <- summary(glm(msi_column ~ name, data=df, family="binomial"))$p.value
-  }
+#Check if MSI score column is binary (use logistic regression) or not (use linear regression)
+if( length(levels(droplevels(as.factor(df[,col]))))==2 ){
+  msi_binary <- TRUE
+} else if( length(levels(droplevels(as.factor(df[,col]))))>2 ){
+  msi_binary <- FALSE
 }
 else{
-  for(name in parameter_names){
-    check name is numeric
-    results_df <- summary(lm(msi_column ~ name, data=df))$p.value
-  }  
+  stop("Whoa, MSI score column should have more than one level.\n") 
 }
-print or write(results_df)
+
+#Vector of parameters
+params <- names(df)[fdc:ncol(df)]
+#store results here
+results <- vector("list", length(params))
+names(results) <- params
+
+for( p in params ){
+  model <- NULL
+  #Run univariate logistic or linear regression
+  if( msi_binary ){ #logistic
+    model <- glm( df[,col] ~ df[,p], data=df, family="logistic")
+  } else if( !msi_binary ){ #linear
+    model <- lm( df[,col] ~ df[,p], data=df)
+  }
+  results[[p]] <- summary(model)
+}
+
+return(results)
