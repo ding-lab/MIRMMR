171d1f9630eb43835c500d9c67eb8675150491c9
envest <stevenmasonfoltz@gmail.com>
2017-01-11 12:16:44 -0600

    Write stepAIC functionality

--- modules/stepwise.R
+++ modules/stepwise.R
@@ -1,12 +1,22 @@
-#Module returns best model using stepwise regression
-msi_column
-first_parameter
-direction (default="both")
-require(MASS)
-if(msi_column is binary){
-  write model statement
-  evalute entire model
-  run stepAIC()
-  return best model and predicted values
+#Module returns best model using stepwise regression (MASS::stepAIC)
+#col <- opt$msi_score
+#fdc <- opt$first_data_column
+#msi_binary is TRUE if only two factors in MSI score column, FALSE otherwise
+
+suppressPackageStartupMessage(library(MASS))
+
+#Model formula
+f <- paste( names(df)[col], paste(names(df)[fdc:ncol(df)], collapse=" + "), sep=" ~ ")
+
+#Fit full model
+if( msi_binary ){ #logistic
+  model <- glm(f, data=df, family="logistic")
+} else if( !msi_binary ){ #linear
+  model <-  lm(f, data=df)
 }
+
+#Find best model by AIC
+stepwise_model <- stepAIC(model, direction="both")
+
+return(stepwise_model)
 
