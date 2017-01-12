39571e4c837981ba116cc5668b8ac35db69e5456
envest <stevenmasonfoltz@gmail.com>
2017-01-12 15:01:11 -0600

    Updated for logitic only, and TODO list for output

--- modules/stepwise.R
+++ modules/stepwise.R
@@ -1,7 +1,4 @@
 #Module returns best model using stepwise regression (MASS::stepAIC)
-#col <- opt$msi_score
-#fdc <- opt$first_data_column
-#msi_binary is TRUE if only two factors in MSI score column, FALSE otherwise
 
 suppressPackageStartupMessage(library(MASS))
 
@@ -9,14 +6,13 @@ suppressPackageStartupMessage(library(MASS))
 f <- paste( names(df)[col], paste(names(df)[fdc:ncol(df)], collapse=" + "), sep=" ~ ")
 
 #Fit full model
-if( msi_binary ){ #logistic
-  model <- glm(f, data=df, family="logistic")
-} else if( !msi_binary ){ #linear
-  model <-  lm(f, data=df)
-}
+model <- glm(f, data=df, family="logistic")
 
 #Find best model by AIC
 stepwise_model <- stepAIC(model, direction="both")
 
-return(stepwise_model)
+#TODO: Predict MSI-H probabilities based on model
+#Calculate AUC, plot ROC, etc. 
+#Write model summary to output
+#Save model in .Robj file
 
