92b348ad2117707fb5a7777f3e86f4d66789e18a
envest <stevenmasonfoltz@gmail.com>
2017-01-12 14:58:22 -0600

    Updated for logistic only

--- modules/univariate.R
+++ modules/univariate.R
@@ -1,7 +1,4 @@
 #Module returns univariate model p-values for each parameter in data frame
-#col <- opt$msi_score
-#fdc <- opt$first_data_column
-#msi_binary is TRUE if only two factors in MSI score column, FALSE otherwise
 
 #Vector of parameters
 params <- names(df)[fdc:ncol(df)]
@@ -12,12 +9,9 @@ names(results) <- params
 for( p in params ){
   model <- NULL
   #Run univariate logistic or linear regression
-  if( msi_binary ){ #logistic
-    model <- glm( df[,col] ~ df[,p], data=df, family="logistic")
-  } else if( !msi_binary ){ #linear
-    model <- lm( df[,col] ~ df[,p], data=df)
-  }
+  model <- glm( df[,col] ~ df[,p], data=df, family="logistic")
   results[[p]] <- summary(model)
 }
 
+#Write results to output 
 return(results)
