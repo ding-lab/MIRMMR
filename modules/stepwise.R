5b8f76110c512bc8063f08a575f2019adb215f23
envest <stevenmasonfoltz@gmail.com>
2017-01-13 12:11:12 -0600

    ROC, AUC, save output

--- modules/stepwise.R
+++ modules/stepwise.R
@@ -12,7 +12,21 @@ model <- glm(f, data=df, family="logistic")
 stepwise_model <- stepAIC(model, direction="both")
 
 #TODO: Predict MSI-H probabilities based on model
-#Calculate AUC, plot ROC, etc. 
-#Write model summary to output
-#Save model in .Robj file
+probs <- predict(stepwise_model, newdata=df, type="response")
 
+#ROC
+roc_df <- roc( data.frame(df[,col], probs) )
+
+#AUC
+auc <- auc( roc_df )
+
+#Plot ROC
+p <- plot_roc( roc_df , "Title", auc)
+
+#Write model summary to output
+dir=opt$output_directory
+fileprefix=opt$output_prefix
+write(x=summary(stepwise_model), file=paste0(output_dir_prefix,".stepwise_model_summary.txt"))
+
+#Save model in .Robj file
+save(stepwise_model, file=paste0(outputdir_prefix,".stepwise_model.Robj"))
