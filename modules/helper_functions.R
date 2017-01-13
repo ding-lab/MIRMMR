cba9afb303d3731ad4fa6868f45325d0fb7b19d8
envest <stevenmasonfoltz@gmail.com>
2017-01-13 11:42:46 -0600

    Lots of sanity_check statements

--- modules/helper_functions.R
+++ modules/helper_functions.R
@@ -1,13 +1,6 @@
 #Set of functions to help facilitate rest of scripts
 
 #Function +++++++++++++++++++++++++++++
-#Check inputs for sanity and correctness, exit if any problem
-sanity_checks <- function(opt){
-  #TODO check all input parameters
-}
-#--------------------------------------
-
-#Function +++++++++++++++++++++++++++++
 #Track how many times each variable appears in models
 consensus_parameters <- function(opt, myX, myY){
   #TODO update opt parameter names
@@ -101,5 +94,120 @@ auc <- function( roc ){
 plot_roc <- function( plot_df ){
   #Function plots a ROC curve given (possible) multiple curves
   #Input is a three column data frame (curve name, tp, fp)
+}
+#--------------------------------------
+
+#Function +++++++++++++++++++++++++++++
+#Check inputs for sanity and correctness, exit if any problem
+sanity_checks <- function(opt){
+  em <- function(old_message, new_message){
+    return( paste0(old_message, new_message, sep="\n") )
+  }
+  error_message <- NULL
+  #error_message <- em(error_message, "Error: ")
+  #Primary checks of module and data_frame
+  #module
+  list_of_modules <- c("compare", "penalized", "stepwise", "univariate")
+  if( !(opt$module %in% list_of_modules) ){
+    error_message <- em(error_message, paste0("Error: module (-m) must be one of ", paste(list_of_modules, collapse=" "),"."))
+  }
+  if( !is.null(error_message) ){
+    stop(error_message)
+  }
+  #data_frame
+  if( is.null(opt$data_frame) ){
+    error_message <- em(error_message, "Error: data frame (-d) is NULL and must be specified.")
+  } else if( !(file.exists(opt$data_frame)) ){
+    error_message <- em(error_message, "Error: data frame (-d) file does not exist.")
+  } else if( !(file.access(opt$data_frame, mode=4)) ){
+    errro_message <- em(error_message, "Error: data frame (-d) file is not readable.")
+  } else if( TRUE ){
+    default_warn <- options()$warn
+    options(warn=2)
+    df <- read.table(opt$data_frame, header=TRUE)
+    options(warn=default_warn)
+  }
+  if( !is.null(error_message) ){
+    stop(error_message)
+  }
+
+  #Secondary checks of required options (msi_status, first_data_column, output_prefix, output_directory, overwrite, plots)
+  #msi_status
+  if( is.null(opt$msi_status) ){
+    error_message <- em(error_message, "Error: MSI status column name (-i) must be specified.")
+  } else if( !(opt$msi_status %in% names(df))  ){
+    error_message <- em(error_message, "Error: MSI status column name (-i) not in data frame column names.")
+  } else if( length(levels(droplevels(as.factor(df[,opt$msi_status]))))!=2 ){
+    error_message <- em(error_message, "Error: MSI status column (-i) may only have two levels.")
+  } 
+  #first_data_column
+  if( is.null(opt$first_data_column) ){
+    error_message <- em(error_message, "Error: Number of first data column (-c) must be specified.")
+  } else if( !is.double(opt$c) || opt$c!=round(opt$c) ){
+    error_message <- em(error_message, "Error: Number of first data column (-c) must be an integer.")
+  } else if( opt$c >= ncol(df) | opt$c <= which(names(df)==opt$msi_status ){
+    error_message <- em(error_message, "Error: Number of first data column (-c) must be greater than the column number of the MSI status column and must be less than number of columns of the input data frame.")
+  }
+  #output_prefix
+  if( is.null(opt$output_prefix) ){
+    error_message <- em(error_message, "Error: Output file prefix (-o) must be specified.")
+  }
+  #output_directory
+  if( is.null(opt$output_directory) ){
+    error_message <- em(error_message, "Error: Output file directory (-d) must be specified.")
+  }
+  #TODO check if file exists and will be overwritten if overwrite=FALSE
+  #plots
+  if( !is.logical(opt$plots) ){
+    error_message <- em(error_message, "Error: Plots (--plots) must be logical TRUE/FALSE, default is FALSE.")
+  }
+  #Exit if error_message not NULL
+  if( !is.null(error_message) ){
+    stop(error_message)
+  }
+
+  #Tertiary chcks of penalized module options (alpha, consensus, lambda, nfolds, repeats, set_seed, train, train_proportion, type_measure)
+  if( opt$module == "penalized" ){
+    #alpha
+    if( !is.double(opt$alpha) || opt$alpha < 0 | opt$alpha > 1 ){
+      error_message <- em(error_message, "Error: Alpha (--alpha) must be a double beteween 0 and 1, default=0.9.")
+    }
+    #consensus
+    if( !is.logical(opt$consensus) ){
+      error_message <- em(error_message, "Error: Consensus (--consensus) must be logical TRUE/FALSE, default is FALSE.")
+    }
+    #lambda
+    if( !(opt$lambda %in% c("lambda.1se","lambda.min")) ){
+      error_message <- em(error_message, "Error: Lambda (--lambda) must be one of \"lambda.1se\" or \"lambda.min\", default is \"lambda.1se\".")
+    }
+    #nfolds
+    if( !is.double(opt$nfolds) || opt$nfolds!=round(opt$nfolds) | opt$nfolds < 1 | opt$nfolds > nrow(df) ){
+      error_message <- em(error_message, "Error: Number of folds (--nfolds) must be an integer between 1 and the number of samples in the input data frame, default is 10.")
+    } 
+    #repeats
+    if( !is.double(opt$repeats) || opt$repeats!=round(opt$repeats) | opt$repeats < 1){
+      error_message <- em(error_message, "Error: Number of repeats (--repeats) must be a positive integer, default is 1000.")
+    }
+    #set_seed
+    if( !(is.logical(opt$set_seed) | is.double(opt$set_seed)) ){
+      error_message <- em(error_message, "Error: Random seed (--seed) must be FALSE, TRUE, or any double, default is FALSE.")
+    }
+    #train
+    if( !is.logical(opt$train) ){
+      error_message <- em(error_message, "Error: Train option (--train) must be logical TRUE or FALSE, default is FALSE.")
+    }
+    #train_proportion
+    if( !is.double(opt$train_proportion) || opt$train_proportion < 0 | opt$train_proportion > 1 ){
+      error_message <- em(error_message, "Error: Train proportion (--train_proportion) must be a double between 0 and 1, default is 0.8.")
+    }
+    #type_measure
+    if( !(opt$type_measure %in% c("mse", "deviance", "mae", "class", "auc")) ){
+      error_message <- em(error_message, "Error: Type measure (--type_measure) must be one of \"mse\", \"deviance\", \"mae\", \"class\", \"auc\", default is \"class\".")
+    }
+  }
+  #Exit if error_message not NULL
+  if( !is.null(error_message) ){
+    stop(error_message)
+  }
 }
 #--------------------------------------
