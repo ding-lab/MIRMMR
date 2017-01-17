05ebe40fdeb2c8e5edb19742113e24ada37bbb90
envest <stevenmasonfoltz@gmail.com>
2017-01-17 10:48:38 -0600

    Title for ROC curve plot

--- modules/stepwise.R
+++ modules/stepwise.R
@@ -7,7 +7,7 @@ suppressPackageStartupMessage(library(MASS))
 f <- paste( names(df)[col], paste(names(df)[fdc:ncol(df)], collapse=" + "), sep=" ~ ")
 
 #Fit full model
-model <- glm(f, data=df, family="logistic")
+model <- glm(f, data=df, family=binomial)
 
 #Find best model by AIC
 stepwise_model <- stepAIC(model, direction="both")
@@ -22,7 +22,7 @@ roc_df <- roc( data.frame(df[,col], probs) )
 auc <- auc( roc_df )
 
 #Plot ROC
-p <- plot_roc( roc_df , "Title", auc)
+p <- plot_roc( roc_df, user_title="ROC curve of stepwise regression model")
 
 #Write model summary to output
 write(summary(stepwise_model), file=paste0(output_dir_prefix,".stepwise_model_summary.txt"))
