0900d24735cdf523934ffc357da3792c0aa2f2cb
envest <stevenmasonfoltz@gmail.com>
2017-05-31 15:50:56 -0500

    Add extra mutation and methylation info to output

--- modules/predict.R
+++ modules/predict.R
@@ -2,7 +2,7 @@
 
 model <- get(load(opt$model))
 if( all( names(model$coefficients)[-1] %in% names(df)[fdc:ncol(df)] )){
-  output <- data.frame(df[,1:(fdc-1)], MIRMMR=predict(model, newdata=df[,fdc:ncol(df)], type="response"))
+  output <- data.frame(df[,1:(fdc-1)], MIRMMR=predict(model, newdata=df[,fdc:ncol(df)], type="response"), df[, names(df) %in% names(model$coefficients)[-1] ])
   write.table(output, file=paste0(output_dir_prefix,".predict.txt"), quote=FALSE, sep="\t", row.names=FALSE, col.names=TRUE)
 
 } else{
